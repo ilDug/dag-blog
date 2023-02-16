e1251544fc9dfbc0ec04fe8adc093d690ac3faa3
ilDug <dognini.marco@gmail.com>
2023-02-16 08:58:58 +0100

    Footer  - modificati link e immagini Aggiunto collegamento a Buy me a coffee.

--- web/pages/views/footer.php
+++ web/pages/views/footer.php
@@ -4,10 +4,8 @@
             <div class="col-sm-3 mb-3">
                 <h5 class="text-muted">Links</h5>
                 <ul>
-                    <li><a href="/contacts">contatti</a></li>
-                    <li><a href="/map">dove trovarmi</a></li>
-                    <li><a href="/creations">creazioni</a></li>
-                    <li><a href="/jobs">cosa posso fare per te</a></li>
+                    <li><a href="/">Blog Home</a></li>
+                    <li><a href="/search">Search</a></li>
                     <li><a href="/privacy/policy">privacy policy</a></li>
                     <li><a href="/privacy/cookies">cookies policy</a></li>
                     <li><a href="#" dcc-open>cookies settings</a> </li>
@@ -15,26 +13,29 @@
             </div>
             <div class="col-sm-6 mb-3 d-flex flex-column justify-content-between">
                 <p>
-                    <a href="https://www.facebook.com/simounichecreazioni/" target="_blank"><i class="fab fa-2x mx-3 fa-facebook"></i></a>
-                    <a href="https://www.instagram.com/simounichecreazioni/" target="_blank"><i class="fab fa-2x mx-3 fa-instagram"></i></a>
-                    <a href="https://www.pinterest.it/simonabarboni/" target="_blank"><i class="fab fa-2x mx-3 fa-pinterest"></i></a>
+                    <a href="https://" target="_blank"><i class="fa-brands fa-2x mx-3 fa-youtube"></i></a>
+                    <a href="https://" target="_blank"><i class="fa-brands fa-2x mx-3 fa-instagram"></i></a>
+                    <a href="https://" target="_blank"><i class="fa-brands fa-2x mx-3 fa-github"></i></a>
+
+                    <a href="https://www.buymeacoffee.com/ilDug" target="_blank"><i class="fa-brands fa-2x mx-3 fa-github"></i></a>
                 </p>
                 <p>
-                    <img src="assets/images/logo-round-dark-full.png" alt="" style="width:5rem;">
+                    <img src="/assets/images/dag-tech-logo-sq.png" alt="Dag Tech" style="width:5rem;">
                 </p>
-                <p class="mb-0">© 2018 - <?php echo date('Y'); ?> Copyright simounichecreazioni.it</p>
+                <p class="mb-0">© 2011 - <?php echo date('Y'); ?> Copyright @ilDug</p>
             </div>
             <div class="col-sm-3 ">
-                <p data-toggle="tooltip" title="scrivimi su whatsapp">
-                    <a href="https://wa.me/+393921373004/" target="_blank">
-                        <i class="fa-brands fa-whatsapp fa-4x"></i> <br><br>
-                        Scrivimi
+                <p data-toggle="tooltip" title="Buy me a coffee">
+                    <a href="https://www.buymeacoffee.com/ilDug" target="_blank">
+                        <span>
+                            <img src="/assets/images/bmc-logo.svg" alt="buy me a coffee" class="img-fluid">
+                        </span>
                     </a>
                 </p>
                 <hr>
                 <p>
-                    <small>Simona Barboni. Vicolo Solferino - 26010 SERGNANO CR - Italia</small>
-                    <small><br> P.IVA: 01696640190</small>
+                    <small>ilDug</small>
+                    <small><br> keep alive Sergnano H.C.</small>
                 </p>
             </div>
         </div>
