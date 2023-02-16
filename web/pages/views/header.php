5374e6f23d43916be54b7ac56695b0d2b4a6f1f1
ilDug <dognini.marco@gmail.com>
2023-02-16 12:03:26 +0100

    inserito offcanvas e mofiicato i links

--- web/pages/views/header.php
+++ web/pages/views/header.php
@@ -1,62 +1,64 @@
 <header>
     <div id="header-banner">
-        <p class="d-none d-md-block">scegli il su misura che ti somiglia!</p>
+        <p class="d-none d-md-block">Dag Blog - mettere typer.js</p>
         <div id="social">
-            <a href="https://wa.me/+393921373004/" target="_blank"><i class="fab  mx-3 fa-whatsapp"></i></a>
-            <a href="https://www.facebook.com/simounichecreazioni/" target="_blank"><i class="fab  mx-3 fa-facebook"></i></a>
-            <a href="https://www.instagram.com/simounichecreazioni/" target="_blank"><i class="fab  mx-3 fa-instagram"></i></a>
-            <a href="https://www.pinterest.it/simonabarboni/" target="_blank"><i class="fab  mx-3 fa-pinterest"></i></a>
+            <a href="https://" target="_blank"><i class="fa-brands fa-2x mx-3 fa-youtube"></i></a>
+            <a href="https://" target="_blank"><i class="fa-brands fa-2x mx-3 fa-instagram"></i></a>
+            <a href="https://" target="_blank"><i class="fa-brands fa-2x mx-3 fa-github"></i></a>
+            <a href="https://www.buymeacoffee.com/ilDug" target="_blank"><i class="fa-regular fa-2x mx-3 fa-cup-togo"></i></a>
         </div>
     </div>
     <div id="header-wrap">
 
         <div id="header-logo">
-            <a href="/"><img src="/assets/images/logo-square-dark.png" alt="" class="img-fluid"></a>
-            <!-- <h4 class="ml-3 mb-0"><a href="/">SIMONA BARBONI <br><small class="text-muted">uniche creazioni</small></a></h4> -->
+            <a href="/"><img src="/assets/images/dag-tech-logo-sq.png" alt="" class="img-fluid"> Dag Blog</a>
         </div>
         <nav class="" id="nav">
             <a href id="menu-o-close"><i class="fal fa-times fa-2x"></i></a>
             <ul>
-                <a href="/about">
+                <a href="/">
                     <li class="" id="nav-about">
-                        <i class="fal fa-hashtag icon mr-1"></i>
-                        Chi sono
+                        <i class="fa-duotone fa-square-terminal mr-1"></i>
+                        blog
                     </li>
                 </a>
-                <a href="/creations">
-                    <li class="" id="nav-creations">
-                        <i class="fal fa-bags-shopping mr-1 icon"></i>
-                        Creazioni
-                    </li>
-                </a>
-                <a href="/contacts">
-                    <li class="" id="nav-contacts">
-                        <i class="fal fa-envelope icon mr-1"></i>
-                        Contatti
-                    </li>
-                </a>
-                <a href="/jobs">
-                    <li class="" id="nav-contacts">
-                        <i class="fal fa-lightbulb-on icon mr-1"></i>
-                        Cosa posso fare
-                    </li>
-                </a>
-                <a href="/map">
-                    <li class="" id="nav-contacts">
-                        <i class="fal fa-map-marked-alt icon mr-1"></i>
-                        Dove trovarmi
-                    </li>
-                </a>
-
             </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+
+
         </nav>
         <div id="header-actions">
             <a href="#" id="header-menu-btn" class="header-action-btn">
                 <i class="fal fa-bars"></i>
             </a>
-            <a href="#" id="header-bigcartel-btn" class="header-action-btn" data-toggle="tooltip">
+            <a href="#" id="header-main-btn" class="header-action-btn" data-toggle="tooltip" data-bs-toggle="offcanvas" href="#offcanvasDag" role="button" aria-controls="offcanvasDag">
                 <i class="fa-brands fa-rebel text-white"></i>
             </a>
+        </div>
+    </div>
+
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasDag" aria-labelledby="offcanvasDagLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasDagLabel">Dag Blog</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <div>
+                Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+            </div>
+            <div class="dropdown mt-3">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                    Dropdown button
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </header>
